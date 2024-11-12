0880c373e9e7fdb0f10711c31aebafe386892eb1
Stuart Forrester <stforrester@gmail.com>
2024-11-12 12:55:22 -0600

    complete the lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// your code here
+
+function handleDelete(event) {
+  event.target.parentNode.remove()
+}
+
+function buildToDo(toDo) {
+  let li = document.createElement('li')
+  let deleteButton = document.createElement('button')
+  deleteButton.textContent = "X"
+  deleteButton.addEventListener('click', handleDelete)
+  li.textContent = `${toDo} `
+  li.appendChild(deleteButton)
+  document.querySelector("#tasks").appendChild(li)
+};
+
+const form = document.querySelector("#create-task-form");
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+  buildToDo(e.target.new_task_description.value)
+  form.reset()
+})
+
